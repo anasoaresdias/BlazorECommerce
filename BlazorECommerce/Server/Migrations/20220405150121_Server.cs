1195c5c604f16c783e001d4dbe425b9b66e95ec2
anabsoar <anabsoar@L-PT-5CG14591J7>
2022-04-05 16:03:11 +0100

    Server

---
+++ BlazorECommerce/Server/Migrations/20220405150121_Server.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorECommerce.Server.Migrations
+{
+    public partial class Server : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
